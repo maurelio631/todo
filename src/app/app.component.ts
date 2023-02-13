e02bc8cb729bcf9e10fa0741574a56c409138955
maurelio631 <marco.silva631@outlook.com>
2023-02-13 10:20:38 -0300

    Adicionando tarefas no array todos e mostrando por meio do ngFor no html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  public todos: any[] = [];
+  public title: string = 'Minhas tarefas';
+  constructor() {
+    this.todos.push('Estudar pelo menos 2h por dia todo dia da semana');
+    this.todos.push('Ir para a academia pelo menos 4x na semana');
+    this.todos.push('Jogar um pouco pelo menos por dia');
+  }
 }
